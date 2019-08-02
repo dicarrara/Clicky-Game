13ed7da42d16223089d13e00986d94c06b40f217
Diana Carrara <dianaVcarrara@gmail.com>
2019-08-01 18:21:05 -0600

    Updates

---
+++ precache-manifest.e269181df4d64e865315000c91e5f7bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5898f12638b046b1802e45a6c58bf05",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "48ce55ef45a7b8f52304",
+    "url": "/clicky-game/static/css/main.4699c900.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "48ce55ef45a7b8f52304",
+    "url": "/clicky-game/static/js/main.e7fdbd88.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
